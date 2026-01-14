05094c99abab9ae48e6e65fe0050faf8e00b22dc
Wern <dev@wern.cc>
2026-01-15 05:58:22 +0800

    display warning for a yabai windows command bug
    yabai occasionally fails to populate `--windows` but will still work
    with `--windows --space N`
    
    but the output of the latter implies yabai believes all windows are
    invisible so it's not very useful anyways
    
    this is weirdly impossible to reproduce so it's hard to figure out why
    
    in any case this adds a warning to hint a restart may be necessary

--- scripts/windows.sh
+++ scripts/windows.sh
@@ -12,6 +12,8 @@ SPACES=$(yabai -m query --spaces 2>&1)
 DISPLAYS=$(yabai -m query --displays 2>&1)
 WINDOWS=$(yabai -m query --windows 2>&1)
 
+WARNING="null"
+
 # check if yabai returned an unexpected item
 # (prefixed with text "yabai-msg:" instead of json object)
 #
@@ -29,11 +31,39 @@ if [[ $WINDOWS == yabai-msg:* ]]; then
   exit 1
 fi
 
+if [[ $WINDOWS == "[]" ]]; then
+  # a bug in yabai may return an empty array for windows
+
+  # in that case we attempt to construct the full list from each space
+  query_window_for_space() {
+    local space_id=$1
+    yabai -m query --windows --space $space_id 2>&1
+  }
+
+  WINDOWS="[]"
+  SPACE_INDICES=$(echo $SPACES | jq '.[].index')
+  for space_id in $SPACE_INDICES; do
+    WINDOWS=$(echo $WINDOWS | jq --argjson new_windows "$(query_window_for_space $space_id)" '. + $new_windows')
+  done
+
+  if [[ $WINDOWS != "[]" ]]; then
+    WARNING="\"clarity-msg: yabai's commands are bugged, restart WM to fix\""
+  fi
+
+  # verify that the final output is valid parsable json
+  if ! echo $WINDOWS | jq . >/dev/null 2>&1; then
+    echo "{\"error\":\"clarity-msg: yabai not outputting windows and clarity workaround failed\"}"
+    exit 1
+  fi
+
+fi
+
 echo $(cat <<-EOF
 {
   "spaces": $SPACES,
   "displays": $DISPLAYS,
-  "windows": $WINDOWS
+  "windows": $WINDOWS,
+  "warning": $WARNING
 }
 EOF
 )
